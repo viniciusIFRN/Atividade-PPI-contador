35ef8744f9da21b1ee6b6a064933a6868f364fb7
vinicius <ferreira.vinicius1@escolar.ifrn.edu.br>
2026-09-15 16:20:30 -0300

    final

--- src/app/item.ts
+++ src/app/item.ts
@@ -1,1 +1,10 @@
-export interface Produto {}
+export interface Produto {
+  id: number;
+  nome: string;
+  preco: number;
+}
+export interface Item {
+  id: number;
+  produto: Produto;
+  quantidade: number;
+}
